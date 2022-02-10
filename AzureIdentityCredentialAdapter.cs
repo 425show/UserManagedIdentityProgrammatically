71d6395cc33e47a55ade4d519d1c2cf069debec9
Awesome Hacker <awesome.hacker@test.com>
2022-02-09 17:45:27 -0800

    First commit

---
+++ AzureIdentityCredentialAdapter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Rest;
+using Azure.Core;
+
+namespace Models
+{
+    public class AzureIdentityCredentialAdapter : TokenCredentials
+    {
+        public AzureIdentityCredentialAdapter(string[] scopes = null) : base(new AzureIdentityTokenProvider(scopes))
+        {
+        }
+
+        public AzureIdentityCredentialAdapter(TokenCredential tokenCredential, string[] scopes = null) : base(new AzureIdentityTokenProvider(tokenCredential, scopes))
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Identity;
+using Microsoft.Azure.Management.ManagedServiceIdentity;
+using Microsoft.Azure.Management.ManagedServiceIdentity.Models;
+using Models;
+
+
+var resourceGroupName = "your resource group name";
+var managedIdentityName = "your managed identity name";
+var subscriptionId = "your Azure Subscription Id";
+var tenantId= "your Azure AD Tenant Id";
+var region = "west-us";
+
+
+var credentials = new DefaultAzureCredential();
+var servCreds = new AzureIdentityCredentialAdapter(credentials);
+var client = new ManagedServiceIdentityClient(servCreds);
+client.SubscriptionId = subscriptionId;
+
+var newUserAssignedIdentity = await client.UserAssignedIdentities.CreateOrUpdateAsync(
+    resourceGroupName: resourceGroupName,
+    resourceName: managedIdentityName,
+    new Identity(
+        location: region,
+        tenantId:new Guid(tenantId)
+        
+    )
+);
+
+//retrieve the managed identity
+var uai = await client.UserAssignedIdentities.GetAsync("identity", "my-managed-identity");
+
+// display the client id
+Console.WriteLine($"User assigned identity: {uai.ClientId}");
